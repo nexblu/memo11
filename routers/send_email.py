dc10fb3b980c1a0c2841d660b4506d098ffdbd94
nexblu <farras.pramudita@gmail.com>
2024-01-18 23:06:42 +0700

    update

--- routers/send_email.py
+++ routers/send_email.py
@@ -9,22 +9,6 @@ import ssl
 from static import Misc
 
 
-def send_email(email):
-    email_address = "ditttt.tiktok@gmail.com"
-    email_password = "orfd xfvf urcj arsb"
-    em = EmailMessage()
-    em["From"] = email_address
-    em["To"] = f"{email}"
-    em["Subject"] = "test"
-    em.set_content("dwawdawadwadwa")
-
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
-        smtp.login(email_address, email_password)
-        smtp.sendmail(email_address, email, em.as_string())
-
-
 load_dotenv()
 
 
