d7557d0680ba8725a45ec8b4a3e28487fe99f868
nexblu <farras.pramudita@gmail.com>
2024-01-18 13:37:17 +0700

    update login page

--- src/components/HomeLogin.jsx
+++ src/components/HomeLogin.jsx
@@ -1,5 +1,6 @@
-import { Image } from 'react-bootstrap'
+import { Image, Button } from 'react-bootstrap'
 import logo from '../../static/images/gbr.png';
+import FormLogin from './FormLogin';
 
 const HomeLogin = () => {
     return (
@@ -8,6 +9,10 @@ const HomeLogin = () => {
                 <h3 className='fw-bold'>Memo 11</h3>
                 <p className='fw-bold'>Login Into Your Memo</p>
                 <Image src={logo}></Image>
+                <FormLogin/>
+                <p>or</p>
+                <button type="button" class="btn btn-warning me-5 ms-5"><p className='text-light fw-bold'>Continue With Google Account</p></button>
+                <p className='mt-2'>DONT HAVE ANT ACCOUNT? <a href="">SIGN UP</a></p>
             </section>
         </>
     )
