6b7dfba0ef516d22f96795b1a8420646ab20f8e0
nexblu <farras.pramudita@gmail.com>
2024-01-18 15:08:14 +0700

    update reactjs

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,19 @@
-import HomeLogin from "../components/HomeLogin";
+import { Image, Button } from 'react-bootstrap'
+import logo from '../../static/images/gbr.png';
+import FormLogin from '../components/FormLogin';
 
 const LoginPage = () => {
     return (
         <>
-            <HomeLogin/>
+            <section className="login jumbotron text-center pt-3">
+                <h3 className='fw-bold'>Memo 11</h3>
+                <p className='fw-bold'>Login Into Your Memo</p>
+                <Image src={logo}></Image>
+                <FormLogin />
+                <p>or</p>
+                <Button variant="warning" class="me-5 ms-5"><p className='text-light fw-bold'>Continue With Google Account</p></Button>{' '}
+                <p className='mt-2'>DONT HAVE ANT ACCOUNT? <a href="">SIGN UP</a></p>
+            </section>
         </>
     )
 }
---
+++ static/script/login.js
@@ -0,0 +1,2 @@
+const pageTitle = `Login Page`;
+document.title = pageTitle;
