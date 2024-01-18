03536c4e45b697bcf9a1373cf44073e511b065e5
nexblu <farras.pramudita@gmail.com>
2024-01-18 13:14:52 +0700

    update reactjs

--- src/components/HomeLogin.jsx
+++ src/components/HomeLogin.jsx
@@ -1,12 +1,13 @@
 import { Button, Image } from 'react-bootstrap'
+import logo from '../../static/images/gbr.png';
 
 const HomeLogin = () => {
     return (
         <>
-            <section className="login jumbotron text-center pt-3 fw-bold">
-                <h3>Memo 11</h3>
-                <p>Login Into Your Memo</p>
-                <Image src='static/images/gbr.png'></Image>
+            <section className="login jumbotron text-center pt-3">
+                <h3 className='fw-bold'>Memo 11</h3>
+                <p className='fw-bold'>Login Into Your Memo</p>
+                <Image src={logo}></Image>
             </section>
         </>
     )
